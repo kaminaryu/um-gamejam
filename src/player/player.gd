24bc937692f4d8cad398814ae6a4fec21b7cf583
saturnSolar <saturnsolar1093@gmail.com>
2026-04-17 23:58:13 +0800

    the

--- src/player/player.gd
+++ src/player/player.gd
@@ -7,20 +7,20 @@ var dash_mult := 1.0
 
 
 func _physics_process(delta: float) -> void :
-	handle_movement(delta)
+    handle_movement(delta)
 
-	
+    
 func handle_movement(delta: float) -> void :
-	var direction := Vector2.ZERO;
-	
-	if (Input.is_action_pressed("move_up")) :
-		direction.y += -1
-	if (Input.is_action_pressed("move_left")) :
-		direction.x += -1
-	if (Input.is_action_pressed("move_down")) :
-		direction.y += 1
-	if (Input.is_action_pressed("move_right")) :
-		direction.x += 1
-	
-	velocity = direction.normalized() * SPEED * dash_mult 
-	move_and_slide()
+    var direction := Vector2.ZERO;
+    
+    if (Input.is_action_pressed("move_up")) :
+        direction.y += -1
+    if (Input.is_action_pressed("move_left")) :
+        direction.x += -1
+    if (Input.is_action_pressed("move_down")) :
+        direction.y += 1
+    if (Input.is_action_pressed("move_right")) :
+        direction.x += 1
+    
+    velocity = direction.normalized() * SPEED * dash_mult 
+    move_and_slide()
