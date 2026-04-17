76d35bcf6be0645c83171732cda575eb5d6e603c
ShmortGuac <aiman17hakimi@gmail.com>
2026-04-17 17:40:58 +0800

    added different type of enemies

--- src/enemy/enemy_base.gd
+++ src/enemy/enemy_base.gd
@@ -3,18 +3,16 @@ extends CharacterBody2D
 class_name Enemy
 
 @export var _enemy_name: String
-@export var _max_health: int
+@export var _health: int
 @export var _damage: int
 @export var _speed: float
-
-var current_health
 
 func set_enemy_name(name: String):
 	_enemy_name = name
 
-func set_max_health(max_health: int):
-	_max_health = max_health
-
+func set_max_health(health: int):
+	_health = health
+	
 func set_damage(damage: int):
 	_damage = damage
 	
@@ -22,8 +20,8 @@ func set_speed(speed: int):
 	_speed = speed
 
 func take_damage(dmg: int):
-	current_health -= dmg
-	if current_health <= 0:
+	_health -= dmg
+	if _health <= 0:
 		die()
 
 func die():
--- src/enemy/enemy_components/damageable_component.gd
+++ src/enemy/enemy_components/damageable_component.gd
@@ -1,10 +1,14 @@
 class_name DamageableComponent
-
 extends Node2D
 
-var parent
+var parent: Enemy
 
 func _ready():
 	parent = get_parent()
+	print(parent._health)
 	
-	
+
+func _on_hitbox_body_entered(body: Node2D) -> void:
+	if(body.is_in_group("Bullet")):
+		print("Enemy got shot")
+		parent.take_damage(5)
