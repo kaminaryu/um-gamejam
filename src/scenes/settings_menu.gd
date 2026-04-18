3b5ad9a2d34c7099fe3bd69fec8f6a3c87e64f24
saturnSolar <saturnsolar1093@gmail.com>
2026-04-18 21:39:43 +0800

    Finalized options menu UI with animations

--- src/scenes/settings_menu.gd
+++ src/scenes/settings_menu.gd
@@ -1,39 +1,47 @@
 extends CanvasLayer
 
 # 1. Reference your slider nodes
-@onready var master_slider = $Control/Master/MasterControl
-@onready var music_slider  = $Control/Music/MusicControl
-@onready var sfx_slider    = $Control/SFX/SFXControl
+@onready var master_slider = $Frame/Contents/ControlMenu/Inputs/MasterControl
+@onready var music_slider  = $Frame/Contents/ControlMenu/Inputs/MusicControl
+@onready var sfx_slider    = $Frame/Contents/ControlMenu/Inputs/SFXControl
+@onready var animation_player = $AnimationPlayer
 
 func _ready() -> void:
-    hide()
-    
+	hide()
+	
 func _input(event: InputEvent) -> void :
-    if (event.is_action_pressed("pause_game")) :
-        close_settings()
-    
+	if (event.is_action_pressed("pause_game")) :
+		close_settings()
+	
 
 func open_settings() -> void:
-    # 2. Sync sliders with the actual audio bus levels
-    master_slider.value = db_to_linear(AudioServer.get_bus_volume_db(AudioServer.get_bus_index(&"Master")))
-    music_slider.value  = db_to_linear(AudioServer.get_bus_volume_db(AudioServer.get_bus_index(&"Music")))
-    sfx_slider.value    = db_to_linear(AudioServer.get_bus_volume_db(AudioServer.get_bus_index(&"SFX")))
-    
-    show()
+	# 2. Sync sliders with the actual audio bus levels
+	master_slider.value = db_to_linear(AudioServer.get_bus_volume_db(AudioServer.get_bus_index(&"Master")))
+	music_slider.value  = db_to_linear(AudioServer.get_bus_volume_db(AudioServer.get_bus_index(&"Music")))
+	sfx_slider.value    = db_to_linear(AudioServer.get_bus_volume_db(AudioServer.get_bus_index(&"SFX")))
+	
+	show()
+	animation_player.play("reveal");
+	
 
 func close_settings() -> void:
-    hide()
+	animation_player.play("close");
 
 func _on_close_pressed() -> void:
-    close_settings()
+	close_settings()
 
 
 # --- Slider Signal Handlers ---
 func _on_master_control_value_changed(value: float) -> void:
-    AudioManager.set_bus_volume_percent(&"Master", value)
+	AudioManager.set_bus_volume_percent(&"Master", value)
 
 func _on_music_control_value_changed(value: float) -> void:
-    AudioManager.set_bus_volume_percent(&"Music", value)
+	AudioManager.set_bus_volume_percent(&"Music", value)
 
 func _on_sfx_control_value_changed(value: float) -> void:
-    AudioManager.set_bus_volume_percent(&"SFX", value)
+	AudioManager.set_bus_volume_percent(&"SFX", value)
+
+
+func _on_animation_player_animation_finished(anim_name: StringName) -> void:
+	if (anim_name == 'close') :
+		hide();
