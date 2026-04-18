62be4f617862903e88313bddc01241781d854aeb
saturnSolar <saturnsolar1093@gmail.com>
2026-04-18 23:03:36 +0800

    Added animation to pause menu

--- src/scenes/pause_menu.gd
+++ src/scenes/pause_menu.gd
@@ -1,13 +1,15 @@
 extends CanvasLayer
 
+@onready var animation_player = $AnimationPlayer
+
 func _on_resume_button_pressed() -> void:
     $ButtonSfx.play_audio()
-    GameMaster.pause_game(false)
+    animation_player.play('close');
 
 func _on_settings_button_pressed() -> void:
     $ButtonSfx.play_audio()
     $SettingsMenu.open_settings()
-        
+
 func _on_restart_button_pressed() -> void:
     WaveHandler.reset()
     GameMaster.reset()
@@ -19,3 +21,7 @@ func _on_menu_button_pressed() -> void:
     GameMaster.reset()
     $ButtonSfx.play_audio()
     get_tree().change_scene_to_file("res://src/scenes/main_menu.tscn")
+
+
+func _on_animation_player_animation_finished(anim_name: StringName) -> void:
+    if (anim_name == 'close') : GameMaster.pause_game(false);
