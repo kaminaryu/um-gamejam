ec853b6672f7e2078ea67b00ab0afb1fa71928a0
IzzAmani <izzryugasaki@gmail.com>
2026-04-18 17:16:28 +0800

    Added Music Controller and Fixed Smth
    Music Player Controller;  Can no longer pause when starting counting down;

---
+++ src/autoloads/music_player.gd
@@ -0,0 +1,26 @@
+extends Node
+
+@onready var music_player := AudioStreamPlayer.new()
+
+const MUSICS = {
+    "Gameplay": preload("res://assets/soundtracks/Bouncing Pyramids.wav"),
+    "Gameover": preload("res://assets/soundtracks/You Lost.wav"),
+}
+
+func _ready() -> void :
+    add_child(music_player)
+    # this is a stringID type shit, instead of checking chars one by one, each string have ID
+    # yk.... LIKE A POINTER
+    music_player.bus = &"Music"
+
+func switch_to_track(name: String) -> void :
+    if not MUSICS.has(name) :
+        push_error("Music Doesnt Exist")
+        return
+        
+    var music_asset = MUSICS[name]
+    if music_player.stream == music_asset and music_player.playing :
+        return
+        
+    music_player.stream = music_asset
+    music_player.play()
--- src/scenes/label.gd
+++ src/scenes/label.gd
@@ -3,45 +3,47 @@ extends Label
 var audio: AudioStreamPlayer
 
 func _ready() -> void:
-	audio = $"../../AudioStreamPlayer"
-	audio.volume_db = -10
-	WaveHandler.increased_wave.connect(_on_wave_increased)
-	# Start the very first sequence 
-	if WaveHandler.wave_number == 1:
-		run_opening_sequence()
-	else:
-		_on_wave_increased()
+    WaveHandler.increased_wave.connect(_on_wave_increased)
+    # Start the very first sequence 
+    if WaveHandler.wave_number == 1:
+        run_opening_sequence()
+    else:
+        _on_wave_increased()
 
 func run_opening_sequence():
-	visible = true
-	# Standard 3, 2, 1, GO loop
-	for i in range(3, 0, -1):
-		display_text(str(i))
-		await get_tree().create_timer(1).timeout
-	display_text("GO!")
-	await get_tree().create_timer(1).timeout
-	
-	WaveHandler.start_wave()
-	visible = false
-	audio.volume_db = 0
+    # dont allow player to pause here
+    #GameMaster.is_ingame = false
+    
+    visible = true
+    # Standard 3, 2, 1, GO loop
+    for i in range(3, 0, -1):
+        display_text(str(i))
+        await get_tree().create_timer(1).timeout
+    display_text("GO!")
+    await get_tree().create_timer(1).timeout
+    
+    WaveHandler.start_wave()
+    visible = false
+    
+    GameMaster.is_ingame = true
 
 func _on_wave_increased() -> void:
-	# This handles Wave 2, 3, etc.
-	if WaveHandler.wave_number > 1:
-		visible = true
-		display_text("WAVE " + str(WaveHandler.wave_number))
-		
-		# Show it for a moment
-		await get_tree().create_timer(1.5).timeout
-		
-		# Start the wave immediately after announcement
-		WaveHandler.start_wave()
-		visible = false
+    # This handles Wave 2, 3, etc.
+    if WaveHandler.wave_number > 1:
+        visible = true
+        display_text("WAVE " + str(WaveHandler.wave_number))
+        
+        # Show it for a moment
+        await get_tree().create_timer(1.5).timeout
+        
+        # Start the wave immediately after announcement
+        WaveHandler.start_wave()
+        visible = false
 
 # Helper function to handle the "Pop" animation so we don't repeat code
 func display_text(new_text: String):
-	text = new_text
-	pivot_offset = size / 2
-	scale = Vector2.ZERO
-	var tween = create_tween()
-	tween.tween_property(self, "scale", Vector2.ONE, 0.5).set_trans(Tween.TRANS_SINE)
+    text = new_text
+    pivot_offset = size / 2
+    scale = Vector2.ZERO
+    var tween = create_tween()
+    tween.tween_property(self, "scale", Vector2.ONE, 0.5).set_trans(Tween.TRANS_SINE)
