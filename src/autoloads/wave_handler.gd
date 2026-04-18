58ea7fffc5b9b339fef69d40cde476d5061d24e6
ShmortGuac <aiman17hakimi@gmail.com>
2026-04-18 21:24:45 +0800

    Improved Gameplay Progression

--- src/autoloads/wave_handler.gd
+++ src/autoloads/wave_handler.gd
@@ -8,14 +8,21 @@ var wave_number: int
 var enemy_count: int
 
 var waves = [
-	{"Green": 5, "Red": 5}, 
-	{"Green": 5, "Red": 5, "Blue": 5, "Yellow": 5},
-	{"Green": 5, "Red": 5, "Split": 5},
-	{"Green": 10, "Blue": 10, "Split": 5},
-	{"Green": 5, "Red": 7, "Yellow": 5, "Split": 5, "Fast": 3},
-	{"Green": 5, "Blue": 5, "Yellow": 2, "Split": 8, "Big": 2,"Fast": 3},
-	{"Green": 5, "Red": 5,"Blue": 5, "Yellow": 5, "Split": 3, "Big": 2,"Fast": 3, "Shoot": 2},
-	{"Green": 10, "Red": 5,"Blue": 5, "Yellow": 5, "Split": 5, "Big": 2,"Fast": 5, "Shoot": 2}
+	{"Green": 5, "Red": 5, "Blue": 0, "Yellow": 0, "Split": 0, "Big": 0, "Fast": 0, "Shoot": 0},
+	{"Green": 5, "Red": 4, "Blue": 2, "Yellow": 0, "Split": 0, "Big": 0, "Fast": 0, "Shoot": 0},
+	{"Green": 4, "Red": 4, "Blue": 3, "Yellow": 0, "Split": 1, "Big": 0, "Fast": 0, "Shoot": 0},
+	{"Green": 3, "Red": 3, "Blue": 3, "Yellow": 2, "Split": 1, "Big": 1, "Fast": 0, "Shoot": 0},
+	{"Green": 3, "Red": 2, "Blue": 3, "Yellow": 2, "Split": 2, "Big": 1, "Fast": 1, "Shoot": 0},
+	{"Green": 2, "Red": 2, "Blue": 3, "Yellow": 3, "Split": 2, "Big": 1, "Fast": 1, "Shoot": 1},
+	{"Green": 2, "Red": 2, "Blue": 3, "Yellow": 4, "Split": 3, "Big": 2, "Fast": 2, "Shoot": 1},
+	{"Green": 0, "Red": 2, "Blue": 3, "Yellow": 4, "Split": 3, "Big": 2, "Fast": 3, "Shoot": 2},
+	{"Green": 0, "Red": 0, "Blue": 2, "Yellow": 4, "Split": 4, "Big": 3, "Fast": 3, "Shoot": 3},
+	{"Green": 0, "Red": 0, "Blue": 2, "Yellow": 4, "Split": 4, "Big": 3, "Fast": 4, "Shoot": 3},
+	{"Green": 0, "Red": 0, "Blue": 1, "Yellow": 3, "Split": 4, "Big": 4, "Fast": 5, "Shoot": 4},
+	{"Green": 0, "Red": 0, "Blue": 0, "Yellow": 3, "Split": 5, "Big": 4, "Fast": 6, "Shoot": 5},
+	{"Green": 0, "Red": 0, "Blue": 0, "Yellow": 2, "Split": 5, "Big": 5, "Fast": 7, "Shoot": 5},
+	{"Green": 0, "Red": 0, "Blue": 0, "Yellow": 0, "Split": 5, "Big": 6, "Fast": 9, "Shoot": 6},
+	{"Green": 0, "Red": 0, "Blue": 0, "Yellow": 0, "Split": 6, "Big": 7, "Fast": 10, "Shoot": 7},
 ]
 
 func _ready() -> void :
