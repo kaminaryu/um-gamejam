3e0956e0a6fb1339e5e70e3d4ed2df379fe3b4d9
saturnSolar <saturnsolar1093@gmail.com>
2026-04-18 21:40:02 +0800

    idfk fuckass godot punye hla

--- src/scenes/pause_menu.gd
+++ src/scenes/pause_menu.gd
@@ -1,17 +1,17 @@
 extends CanvasLayer
 
 func _on_resume_button_pressed() -> void:
-    GameMaster.pause_game(false)
+	GameMaster.pause_game(false)
 
 func _on_settings_button_pressed() -> void:
-    $SettingsMenu.open_settings()
-        
+	$SettingsMenu.open_settings()
+		
 func _on_restart_button_pressed() -> void:
-    WaveHandler.reset()
-    GameMaster.reset()
-    get_tree().change_scene_to_file("res://src/scenes/main.tscn")
+	WaveHandler.reset()
+	GameMaster.reset()
+	get_tree().change_scene_to_file("res://src/scenes/main.tscn")
 
 func _on_menu_button_pressed() -> void:
-    WaveHandler.reset()
-    GameMaster.reset()
-    get_tree().change_scene_to_file("res://src/scenes/main_menu.tscn")
+	WaveHandler.reset()
+	GameMaster.reset()
+	get_tree().change_scene_to_file("res://src/scenes/main_menu.tscn")
