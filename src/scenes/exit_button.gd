c0409f16f7f27f95e33c73fe37e04747465d8c95
saturnSolar <saturnsolar1093@gmail.com>
2026-04-19 02:30:57 +0800

    Added exit button LAT MINUTE

---
+++ src/scenes/exit_button.gd
@@ -0,0 +1,14 @@
+extends TextureButton
+
+
+func _on_mouse_entered() -> void:
+    modulate = Color(1.5, 1.5, 1.5, 1.0)
+
+
+func _on_mouse_exited() -> void:
+    modulate = Color(1.0, 1.0, 1.0, 1.0)
+
+
+
+func _on_pressed() -> void:
+    get_tree().quit();
