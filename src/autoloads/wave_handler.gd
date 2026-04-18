7c41b4787f57f32f91459774841e09f9c329a93f
IzzAmani <izzryugasaki@gmail.com>
2026-04-18 23:33:11 +0800

    fixed wave mechanis not spawning ???

--- src/autoloads/wave_handler.gd
+++ src/autoloads/wave_handler.gd
@@ -1,9 +1,12 @@
 extends Node
+
 signal increased_wave
 signal started_wave
 signal finished_wave
+
 var wave_number: int
 var enemy_count: int
+
 var waves = [
     {"Green": 5, "Red": 5, "Blue": 0, "Yellow": 0, "Split": 0, "Big": 0, "Fast": 0, "Shoot": 0},
     {"Green": 5, "Red": 4, "Blue": 2, "Yellow": 0, "Split": 0, "Big": 0, "Fast": 0, "Shoot": 0},
@@ -21,36 +24,31 @@ var waves = [
     {"Green": 0, "Red": 0, "Blue": 0, "Yellow": 0, "Split": 5, "Big": 6, "Fast": 9, "Shoot": 6},
     {"Green": 0, "Red": 0, "Blue": 0, "Yellow": 0, "Split": 6, "Big": 7, "Fast": 10, "Shoot": 7},
 ]
+
 func _ready() -> void :
     print("wave is ready")
     reset()
-
 
 func reset() -> void :
     wave_number = 1
     enemy_count = 0
 
-
 func register_enemy():
     enemy_count+=1
-
-
+    
 func enemy_defeated():
     enemy_count-=1
     print("Enemy Left: ", enemy_count)
     if enemy_count <= 0:
         finished_wave.emit()
         increase_wave()
-        var sfx = SfxPlayer.get_audio("WaveCompleted")
-        _play_global_sfx(sfx)
-
 
 func _input(event: InputEvent) -> void :
     if (event.is_action_pressed("(DEBUG)IncreaseWave")) :
         increase_wave()
         start_wave()
-
-
+        
+        
 func increase_wave() -> void :
     wave_number += 1
     print("Wave: ", wave_number)
@@ -59,7 +57,6 @@ func increase_wave() -> void :
     await get_tree().create_timer(2.0).timeout
     start_wave()
 
-
 func start_wave():
     print("Wave Start")
     var data: Dictionary
@@ -67,17 +64,4 @@ func start_wave():
         data = waves[wave_number-1]
     else:
         data = waves[waves.size()-1]
-        started_wave.emit(data)
-
-
-func _play_global_sfx(stream: AudioStream):
-    if stream:
-        var player = AudioStreamPlayer.new()
-        player.stream = stream
-        player.pitch_scale = randf_range(0.9, 1.1)
-        
-        player.bus = &"SFX"
-        get_tree().root.add_child(player)
-        
-        player.play()
-        player.finished.connect(player.queue_free)
+    started_wave.emit(data)
