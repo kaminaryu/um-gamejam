809ac719ebbb00a855045824ee7045135976733d
ShmortGuac <aiman17hakimi@gmail.com>
2026-04-19 01:16:57 +0800

    Added Tutorial Description

--- src/enemy/enemy_components/shoot_component.gd
+++ src/enemy/enemy_components/shoot_component.gd
@@ -11,41 +11,41 @@ var timer: Timer
 var target: Node2D
 
 func _ready() -> void:
-	target = get_tree().get_first_node_in_group("Player")
-	
-	# Setup the internal timer
-	timer = Timer.new()
-	add_child(timer)
-	timer.wait_time = shoot_interval
-	timer.autostart = true
-	timer.timeout.connect(_on_timer_timeout)
-	timer.start()
+    target = get_tree().get_first_node_in_group("Player")
+    
+    # Setup the internal timer
+    timer = Timer.new()
+    add_child(timer)
+    timer.wait_time = shoot_interval
+    timer.autostart = true
+    timer.timeout.connect(_on_timer_timeout)
+    timer.start()
 
 func _on_timer_timeout() -> void:
-	if target and is_instance_valid(target):
-		shoot()
+    if target and is_instance_valid(target):
+        shoot()
 
 func shoot() -> void:
-	if bullet_scene == null: return
+    if bullet_scene == null: return
 
-	var bullet = bullet_scene.instantiate()
-	
-	var sprite = bullet.get_node("Sprite2D")
-	# Calculate direction to player
-	var direction = global_position.direction_to(target.global_position)
-	
-	sprite.modulate = Color("#49ab7b")
-	
-	var spawn_offset = direction * 50.0 
-	bullet.global_position = global_position + spawn_offset
-	
-	get_tree().current_scene.add_child(bullet)
-	
-	# Point the bullet
-	bullet.rotation = direction.angle()
-	if "velocity" in bullet:
-		bullet.velocity = direction * bullet_speed
+    var bullet = bullet_scene.instantiate()
+    
+    var sprite = bullet.get_node("Sprite2D")
+    # Calculate direction to player
+    var direction = global_position.direction_to(target.global_position)
+    
+    sprite.modulate = Color("#49ab7b")
+    
+    var spawn_offset = direction * 50.0 
+    bullet.global_position = global_position + spawn_offset
+    
+    get_tree().current_scene.add_child(bullet)
+    
+    # Point the bullet
+    bullet.rotation = direction.angle()
+    if "velocity" in bullet:
+        bullet.velocity = direction * bullet_speed
 
-	# THE PHYSICS FIX: Tell the bullet to ignore the enemy
-	# move_and_collide will now ignore the parent enemy body
-	bullet.add_collision_exception_with(get_parent())
+    # THE PHYSICS FIX: Tell the bullet to ignore the enemy
+    # move_and_collide will now ignore the parent enemy body
+    bullet.add_collision_exception_with(get_parent())
--- src/ui/control_icons.gd
+++ src/ui/control_icons.gd
@@ -5,21 +5,21 @@ var mech_handler: Node2D
 var mech_dict: Dictionary
 
 func _ready():
-	WaveHandler.started_wave.connect(_on_next_wave)
-	player = get_tree().get_first_node_in_group("Player")
-	mech_handler = player.get_node("MechanismHandler")
-	mech_dict = mech_handler.mechanism_controllable
+    WaveHandler.started_wave.connect(_on_next_wave)
+    player = get_tree().get_first_node_in_group("Player")
+    mech_handler = player.get_node("MechanismHandler")
+    mech_dict = mech_handler.mechanism_controllable
 
 func _on_next_wave(data):
-	var mech_arr = [
-		mech_dict["Aiming"]["enabled"], 
-		mech_dict["Dash"]["enabled"], 
-		mech_dict["Shooting"]["enabled"], 
-		mech_dict["Vision"]["enabled"]
-	]
+    var mech_arr = [
+        mech_dict["Aiming"]["enabled"], 
+        mech_dict["Dash"]["enabled"], 
+        mech_dict["Shooting"]["enabled"], 
+        mech_dict["Vision"]["enabled"]
+    ]
    
-	var icons = get_children()
-	for i in range(icons.size()):
-		var x_node = icons[i].get_node_or_null("X")
-		if x_node:
-			x_node.visible = not mech_arr[i]
+    var icons = get_children()
+    for i in range(icons.size()):
+        var x_node = icons[i].get_node_or_null("X")
+        if x_node:
+            x_node.visible = not mech_arr[i]
