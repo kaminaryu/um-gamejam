c5c310756ab15f91f8b2f70a2222321b9c7f9376
IzzAmani <izzryugasaki@gmail.com>
2026-05-14 19:09:54 +0800

    Converted to Godot 4.6
    Genuienly dont know what changed tbh

--- src/scenes/close.gd
+++ src/scenes/close.gd
@@ -1,8 +1,8 @@
 extends TextureButton
 
 func _on_mouse_entered() -> void:
-    modulate = Color(1.5, 1.5, 1.5, 1.0)
+	modulate = Color(1.5, 1.5, 1.5, 1.0)
 
 
 func _on_mouse_exited() -> void:
-    modulate = Color(1.0, 1.0, 1.0, 1.0)
+	modulate = Color(1.0, 1.0, 1.0, 1.0)
