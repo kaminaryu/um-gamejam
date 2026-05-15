86d1dfe43b88f315e12060a44238f2544d4461ac
IzzAmani <izzryugasaki@gmail.com>
2026-05-16 03:52:31 +0800

    Changed Weightage and Also Shooting Delay

--- src/mechanism/shooting/uncontrolled.gd
+++ src/mechanism/shooting/uncontrolled.gd
@@ -1,16 +1,16 @@
 extends Node2D
 
 func enable(enable: bool) -> void :
-    if (enable) :
-        process_mode = Node.PROCESS_MODE_INHERIT
-        $Timer.start(0.5)
-    else :
-        process_mode = Node.PROCESS_MODE_DISABLED
-        $Timer.stop()
+	if (enable) :
+		process_mode = Node.PROCESS_MODE_INHERIT
+		$Timer.start(0.5)
+	else :
+		process_mode = Node.PROCESS_MODE_DISABLED
+		$Timer.stop()
 
 
 func _on_timer_timeout() -> void:
-    var random_time := randf_range(0.3, 0.67)
-    $Timer.start(random_time)
-    
-    get_parent().shoot()
+	var random_time := randf_range(0.1, 1.1)
+	$Timer.start(random_time)
+	
+	get_parent().shoot()
